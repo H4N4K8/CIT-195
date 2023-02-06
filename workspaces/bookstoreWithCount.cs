957597a3a10e7b04650c73f9270ce31bbd66728b
H4N4K8 <123414820+H4N4K8@users.noreply.github.com>
2023-02-06 16:32:41 -0500

    Create bookstoreWithCount.cs

---
+++ workspaces/bookstoreWithCount.cs
@@ -0,0 +1,105 @@
+using System;
+
+namespace bookStore
+{
+    class book()
+    {
+        private int Id;
+        private string Title;
+        private string Author;
+        private static int _transactions=0;
+
+        public void SetTrans()
+        {
+            _transactions++;
+        }
+        public void GetTrans()
+        {
+            return _transactions;
+        }
+        public int Id()
+        {
+            get
+            {
+            return this.Id
+            }
+            set
+            {
+            this.Id;
+            }
+        }
+
+        public string Title()
+        {
+            get
+            {
+            return this.Title
+            }
+            set
+            {
+            this.Title;
+            }
+            Console.WriteLine(Title);
+        }
+
+        public string Author()
+        {
+            get
+            {
+            return this.Author
+            }
+            set
+            {
+            this.Author;
+            }
+            Console.WriteLine(Author);
+        }
+    }
+    class myStore
+    {
+
+        static void Main(string[] args)
+        {
+            book book1 = new book();
+            book1.SetTrans();
+            book1.SetId(1);
+            book1.SetTitle("The Cool Goose Meets A Moose");
+            book1.SetAuthor("Trucie Goosie");
+
+            book book2 = new book();
+            book2.SetTrans();
+            Console.WriteLine("Please enter the member ID: ");
+            book2.SetId(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Please enter the first name: ");
+            book2.SetTitle(Console.ReadLine());
+            Console.WriteLine("Please enter the last name: ");
+            book2.SetAuthor(Console.ReadLine());
+
+            book book3 = new book(3, "Freddy's Fantastic Folk Tales", "Freddy F. Bear");
+            book3.SetTrans();
+
+            Console.WriteLine("Please enter the member ID: ");
+            int tempID = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the first name: ");
+            string tempTitle = Console.ReadLine();
+            Console.WriteLine("Please enter the last name: ");
+            string tempAuthor = Console.ReadLine();
+            book book4 = new book(tempID, tempTitle, tempAuthor);
+            book4.SetTrans();
+
+            Console.WriteLine("The book store has {book1.GetTrans()} books");
+            displayBooks(book1);
+            displayBooks(book2);
+            displayBooks(book3);
+            displayBooks(book4);
+        }
+        static void displayBooks(book book1)
+        {
+        Console.WriteLine("Here's your book information");
+        Console.WriteLine($"Book ID: {book1.GetId()}");
+        Console.WriteLine($"Book Name: {book1.GetTitle()}");
+        Console.WriteLine($"Book Name: {book1.GetAuthor()}");
+        }
+
+    }
+}
