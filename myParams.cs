5868a25fbd0a8e85867865c0ab350e85d6dc6764
H4N4K8 <123414820+H4N4K8@users.noreply.github.com>
2023-02-13 15:57:55 -0500

    Add files via upload

---
+++ myParams.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("How many numbers to generate?");
+        int size = Console.ReadLine();
+        Random r = new Random();
+        int[] numbers = { r.Next(1, 10) * size};
+        Console.WriteLine($"Total of the numbers array={Add(numbers)}");
+
+    }
+    static int Add(params int[] numbers)
+    {
+        int total = 0;
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+        return total;
+    }
+    static int Multiply(params int[] numbers)
+    {
+        int total = 1;
+        foreach (int number in numbers)
+        {
+            total * number;
+        }
+    }
+}
